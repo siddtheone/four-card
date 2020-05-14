0b42760875ea4ad41425c1e85546107d9cbf28d7
Siddharth Likhar <siddharthl@cybage.com>
2020-05-14 18:54:11 +0530

    Updates

---
+++ precache-manifest.d3c1afb7aacbb027baf77beec695dca3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11f94c39d1ba6235a673b49d3af7cbc4",
+    "url": "/four-card/index.html"
+  },
+  {
+    "revision": "48f7521334df4613747b",
+    "url": "/four-card/static/css/main.54dc5ae9.chunk.css"
+  },
+  {
+    "revision": "aa0a5664c89dad0500fc",
+    "url": "/four-card/static/js/2.eb2330bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/four-card/static/js/2.eb2330bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48f7521334df4613747b",
+    "url": "/four-card/static/js/main.ba4e7f4a.chunk.js"
+  },
+  {
+    "revision": "9a6849fff4ee617d5296",
+    "url": "/four-card/static/js/runtime-main.ce6e4b18.js"
+  },
+  {
+    "revision": "8e4a5574d019096b62a948e62e920308",
+    "url": "/four-card/static/media/icon-calculator.8e4a5574.svg"
+  },
+  {
+    "revision": "ae2dc13dd8be5d72dc6ea67236f8729c",
+    "url": "/four-card/static/media/icon-karma.ae2dc13d.svg"
+  },
+  {
+    "revision": "8f7824b371e618577b20ba3775d9cfb4",
+    "url": "/four-card/static/media/icon-supervisor.8f7824b3.svg"
+  },
+  {
+    "revision": "8a1a6a173b407f9e45ba99fb7f25f0db",
+    "url": "/four-card/static/media/icon-team-builder.8a1a6a17.svg"
+  }
+]);
